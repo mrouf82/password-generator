5d1d16fbab99d3431283176b72df0823d5508d6d
Mohammad Rouf <mrouf82@gmail.com>
2021-01-10 22:57:19 -0500

    edited html to include criterias

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,8 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  var letterChar = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+  var letterUpperChar = [A,B,C,D,E,F,G,H,I,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
   passwordText.value = password;
 //Math.floor(Math.random()*99999999)+1
 }
